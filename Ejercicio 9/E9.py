a81fda97b679b5543f781d7f4f1b66327b2cda62
MarceLCF <70829087+MarceLCF@users.noreply.github.com>
2023-10-15 21:40:49 -0400

    Update E9.py

--- Ejercicio 9/E9.py
+++ Ejercicio 9/E9.py
@@ -1,34 +1,33 @@
-import random
 from sklearn.datasets import load_iris
 
-# Cargar el conjunto de datos Iris
-iris = load_iris()
-data = iris.data
-target = iris.target
+datos= load_iris()
+x = datos.data
+y = datos.target
 
-# Obtener el número total de muestras
-total_samples = len(data)
+# cantidad  total de muestras
+cant = len(x)
 
-# Definir el tamaño del conjunto de entrenamiento y prueba
-train_size = int(0.8 * total_samples)
-test_size = total_samples - train_size
+# Definir el tamaño del train y test
+train_size = int(0.8 * cant)
+test_size = cant - train_size
 
-# Crear una lista de índices únicos
-indices = list(range(total_samples))
+# lista de índices
+indices = list(range(cant))
 
-# Mezclar los índices para asegurar que los datos de entrenamiento y prueba sean aleatorios
+import random
+#mezclando los indices para que los dstos de train y test sean aleatorios
 random.shuffle(indices)
 
-# Dividir los índices en conjuntos de entrenamiento y prueba
-train_indices = indices[:train_size]
-test_indices = indices[train_size:]
+# Dividir los índices entrenamiento y prueba
+train_ind = indices[:train_size]
+test_ind = indices[train_size:]
 
-# Crear los conjuntos de entrenamiento y prueba
-train_data = [data[i] for i in train_indices]
-train_target = [target[i] for i in train_indices]
+# se crean los conjuntos de train y test
+train_data = [x[i] for i in train_ind]
+train_target = [y[i] for i in train_ind]
 
-test_data = [data[i] for i in test_indices]
-test_target = [target[i] for i in test_indices]
+test_data = [x[i] for i in test_ind]
+test_target = [y[i] for i in test_ind]
 
-print("Tamaño de los datos de entrenamiento:", len(train_data))
-print("Tamaño de los datos de prueba:", len(test_data))
+print("Tamaño del train:", len(train_data))
+print("Tamaño del test:", len(test_data))
